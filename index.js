0f6c7f9b7d9c6ddaad99e27148115c16e0ab6761
Bala Krishna <balakrishnasadu.89@gmail.com>
2015-07-09 16:47:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 var Canvas = require('canvas');
 var Image = Canvas.Image;
 
-module.exports = {
-	createCaptcha : function() {
+var mode = 1,
+    captchaCode = [],
+    alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 
-		// below code generates only alphaNumeric captcha code but not an image
-		var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-		var a = Math.ceil((Math.random() * alpha.length) / 10);
-		var b = alpha[Math.floor(Math.random() * alpha.length)];
-		var c = alpha[Math.floor(Math.random() * alpha.length)];
-		var d = Math.ceil((Math.random() * alpha.length) / 10);
-		var e = alpha[Math.floor(Math.random() * alpha.length)];
-		var f = Math.ceil((Math.random() * alpha.length) / 10);
-		var g = Math.ceil((Math.random() * alpha.length) / 10);
-		var code = a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
+module.exports = {
+	createCaptcha : function(object) {
+		if (object.mode == 1) {
+			// below code generates numeric captcha code
+			var code = '';
+			for (var i = 0; i < object.length; i++) {
+				code = code + Math.floor((Math.random() * 99) / 10);
+			}
+		} else if (object.mode == 2) {
+			// below code generates alphabetical captcha code
+			var code = '';
+			for (var i = 0; i < object.length; i++) {
+				code = code + alpha[Math.floor(Math.random() * alpha.length)];
+			}
+		} else {
+			// below code generates alphaNumeric captcha code
+			var code = '';
+			for (var i = 0; i < object.length; i++) {
+				if (i % 2 == 0) {
+					code = code + alpha[Math.floor(Math.random() * alpha.length)];
+				} else {
+					code = code + Math.ceil((Math.random() * alpha.length) / 10);
+				}
+			}
+		}
 
 		var canvas = new Canvas(125, 40);
 		var ctx = canvas.getContext('2d');
@@ -22,7 +38,6 @@ module.exports = {
 		ctx.rotate(.1);
 		ctx.fillText(code, 5, 20);
 
-		var captchaCode = [];
 		captchaCode.push(code);
 		captchaCode.push(canvas.toDataURL());
 		return captchaCode;
